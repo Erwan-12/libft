7f384607cec11126677ae816cb42bd62c92c29b1
Erwan Fontaine <erwfonta@made-f0Dr10s18.clusters.42paris.fr>
2024-05-27 12:25:08 +0200

    finished

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: erwfonta <erwfonta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 16:47:23 by erwfonta          #+#    #+#             */
-/*   Updated: 2024/05/27 10:02:18 by erwfonta         ###   ########.fr       */
+/*   Updated: 2024/05/27 11:09:07 by erwfonta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp_lst;
 
-	if (lst != NULL)
+	while (*lst != NULL)
 	{
-		while (*lst != NULL)
-		{
-			tmp_lst = (*lst)->next;
-			ft_lstdelone(*lst, del);
-			*lst = tmp_lst;
-		}
+		tmp_lst = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp_lst;
 	}
 }
